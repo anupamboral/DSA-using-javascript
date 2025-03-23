93350093c6d7ec1f5c57d2c9301ef79a372b388d
anupamboral <anupamboral6889@gmail.com>
2025-03-23 21:23:35 +0530

    Definition of time and space complexity and O(n) explanation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//*
+//* Data Structure:-  A data structure is a specific way of organizing storing and accessing data.
+// * Algorithm:-  A set of instructions that tells a computer how to do something or you can fulfil step by step solution of the problem is called algorithm
+//* By learning data structure and algorithm we can become great problem solver
+//* How can we tell if the code we write is good code or bad code?Or, what is space complexity ond time complexity of your code?Or, what is the Big O of your code?
+//* So the answer of all of the above questions is "Big O Notation" .
+//* Big O Notation:- Big O notation helps us to understand how much time an algorithm will take to run or how much memory it will need as the amount of data it handles grows.
+//* here the first tells about the time complexity and second part tell;s about the space complexity.
+//* Time complexity:-Time complexity helps us to understand how much time an algorithm will take to run .
+//*Space complexity:-Space complexity helps us to understand  how much memory it will need as the amount of data it handles grows
+//* and by combining both of this terms it becomes the Big O Notation
+//* In simple language , We have giant mess to clean ,Big O Nation is like saying how the cleaning time grows as the mess gets bigger.
+//* So the first Big O Notation we are gonna discuss is O(n)- we call it O of n.
+//* O(n)- It signifies that the execution time of the algorithm grows linearly in proportion to the size of the input data(n).
+//* in simple word, as the number of items in the input data increases , the time it takes for the algorithm to run increases correspondingly.
+//* Imagine you have a list of groceries, to find specific items like milk, you might need to scan through the entire list but if the list has five items it takes relatively short time but if the list has 500 items it will take considerably longer time this is the essence of linear time complexity O(n).
+//* ["eggs","Bread","Strawberry","Milk","Bananas"];
+//* Suppose the list of groceries is above array so when the algorithm is trying to find the item from this list let's say we're trying to find milk Then when the algorithm will run first it will go to the The zeroth index when it will not find milk then it will go to the first index then it will go to the third second Index then when it will go to the third index and it will find milk then it will return it so the programme was trying to find milk from this array and as this was a list of five items it took some time to find that item but it would take so much more time if the list would have 5000 items or 5,00,000 items and that's why the big of this code is O(n), Because as the input data increases the algorithm running time also increases correspondingly.
